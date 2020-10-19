49ed7742318981fd5825d3dc34e4b70be86e6551
Daazzer <Daazzer@github.com>
2020-10-19 18:15:34 +0800

    登录页样式效果实现，输入框的封装

---
+++ src/components/ToutiaoInput.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="input-group">
+    <input
+      :class="{
+        'toutiao-input': status === 0 || true,
+        'toutiao-input-success': status === 1,
+        'toutiao-input-danger': status === 2,
+      }"
+      v-bind="$attrs"
+      :value="value"
+      @input="handleInput"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
+    <p class="input-tips" v-show="isShowTips">{{ tipsMsg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: 'ToutiaoInput',
+  props: {
+    value: {
+      type: String
+    },
+    rules: {
+      type: RegExp,
+      required: true
+    },
+    tipsMsg: {
+      type: String,
+      default: '请输入内容'
+    }
+  },
+  data () {
+    return {
+      status: 0,
+      isShowTips: false
+    }
+  },
+  methods: {
+    handleInput (e) {
+      this.$emit('input', e.target.value)
+
+      const inputText = e.target.value.trim()
+      const rules = this.rules
+      const isValidated = rules.test(inputText)
+      if (isValidated) {
+        this.status = 1
+      } else if (inputText === '') {
+        this.status = 0
+      } else {
+        this.status = 2
+      }
+    },
+    handleFocus () {
+      this.isShowTips = true
+    },
+    handleBlur () {
+      this.isShowTips = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input-group {
+  margin-bottom: 0.555556rem;
+}
+.toutiao-input {
+  display: block;
+  width: 100%;
+  padding: 0.416667rem 0;
+  margin-bottom: 0.277778rem;
+  border-bottom: 2px solid #757575;
+  background-color: #f2f2f2;
+  font-size: 0.5rem;
+
+  &-success {
+    border-bottom-color: #0dbc79;
+  }
+  &-danger {
+    border-bottom-color: #ff4d4d;
+  }
+}
+.input-tips {
+  color: #949494;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,20 +7,57 @@
       <span class="iconfont iconnew"></span>
     </div>
     <form class="login-form">
-      <input placeholder="用户名" maxlength="11" />
-      <input placeholder="密码" type="password" />
+      <ToutiaoInput
+        placeholder="用户名/手机号"
+        type="text"
+        v-model="user.name"
+        :rules="/(?:^[A-Za-z]\w{4,5}$|^1\d{10}$)/"
+        maxlength="11"
+        tipsMsg="请输入首字母开头的用户名或11位手机号码"
+      />
+      <ToutiaoInput
+        placeholder="密码"
+        type="password"
+        v-model="user.password"
+        :rules="/(?:^\w{8,16}$)/"
+        maxlength="16"
+        tipsMsg="请输入8-16位大小写字母与数字混合的密码"
+      />
       <p class="tips">
         没有账号？
         <a href="#/register" class="">去注册</a>
       </p>
-      <button class="login-btn" type="submit">登录</button>
+      <Button class="login-btn" native-type="submit" @click="login">登录</Button>
     </form>
   </div>
 </template>
 
 <script>
+import ToutiaoInput from '@/components/ToutiaoInput.vue'
+import Vue from 'vue'
+import { Button } from 'vant'
+
+Vue.use(Button)
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {
+    ToutiaoInput,
+    Button
+  },
+  data () {
+    return {
+      user: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login (e) {
+      e.preventDefault()
+    }
+  }
 }
 </script>
 
@@ -49,19 +86,9 @@ $page: login;
 }
 
 .#{$page}-form {
-  input {
-    display: block;
-    width: 100%;
-    margin-bottom: 0.555556rem;
-    padding: 0.416667rem 0;
-    border-bottom: 2px solid #757575;
-    background-color: #f2f2f2;
-    font-size: 0.5rem;
-  }
-
   .tips {
     text-align: right;
-    margin-bottom: 0.555556rem;
+    margin-bottom: 1rem;
   }
 
   .#{$page}-btn {
