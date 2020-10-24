835e9271794ce50e79cf56c835710149161a946a
Daazzer <Daazzer@github.com>
2020-10-24 23:07:19 +0800

    新闻详情页面样式结构添加

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="articaldetail">
+  <div class="artical-detail">
     <VanNavBar class="header" left-arrow>
       <template #left>
         <van-icon class="icon-back" name="arrow-left back" @click="$router.back()" />
@@ -10,38 +10,46 @@
       </template>
     </VanNavBar>
     <main class="detail">
-      <div class="title">标题</div>
+      <h1 class="title">标题</h1>
       <div class="desc">
-        <span>火星人</span> &nbsp;&nbsp;
-        <span>2019-9-9</span>
+        <span class="publisher">火星人</span>
+        <span class="date">2019-9-9</span>
       </div>
       <div class="content">
         文章的内容：
       </div>
       <div class="opt">
         <span class="like">
-          <van-icon name="good-job-o" />点赞
+          <van-icon name="good-job-o" class="opt_icon" /><span class="like_num">122</span>
         </span>
         <span class="chat">
-          <van-icon name="chat" class="w" />微信
+          <van-icon name="chat" class="opt_icon weixin" />微信
         </span>
       </div>
     </main>
     <!-- 精彩跟帖 -->
     <div class="keeps">
       <h2>精彩跟帖</h2>
-      <div class="item">
-        <div class="head">
-          <img src="../assets/logo.png" alt />
-          <div>
-            <p>火星网友</p>
-            <span>2小时前</span>
+      <div class="comment">
+        <div class="comment_head">
+          <img src="http://localhost:3000/uploads/image/default.jpeg" alt="用户头像" />
+          <div class="comment_user-info">
+            <h3>火星网友</h3>
+            <span class="comment_time">2小时前</span>
           </div>
-          <span>回复</span>
+          <span class="comment_reply">回复</span>
         </div>
-        <div class="text">文章说得很有道理</div>
+        <p class="comment_text">文章说得很有道理</p>
       </div>
-      <div class="more">更多跟帖</div>
+      <van-button round class="more">更多跟帖</van-button>
+    </div>
+    <div class="leave-message">
+      <input type="text" placeholder="写跟帖">
+      <div class="btn-group">
+        <button class="iconfont iconpinglun"></button>
+        <button class="iconfont iconshoucang"></button>
+        <button class="iconfont iconfenxiang"></button>
+      </div>
     </div>
   </div>
 </template>
@@ -84,23 +92,26 @@ export default {
 }
 
 .detail {
-  padding: 15px;
+  padding: common.baseSize(15);
   .title {
-    font-size: 18px;
+    font-size: common.baseSize(20);
     font-weight: bold;
-    padding: 10px 0;
+    padding: common.baseSize(10) 0;
   }
   .desc {
-    line-height: 30px;
+    line-height: common.baseSize(30);
     color: #999;
-    font-size: 13px;
+    font-size: common.baseSize(14);
+    .publisher {
+      margin-right: common.baseSize(10);
+    }
   }
   .content {
-    text-indent: 2em;
-    line-height: 24px;
-    font-size: 15px;
-    padding-bottom: 30px;
     width: 100%;
+    padding-bottom: common.baseSize(30);
+    line-height: common.baseSize(24);
+    font-size: common.baseSize(15);
+    text-indent: 2em;
   }
 }
 .opt {
@@ -108,69 +119,109 @@ export default {
   justify-content: space-around;
   .like,
   .chat {
-    height: 25px;
-    padding: 0 15px;
-    font-size: 14px;
-    line-height: 25px;
-    text-align: center;
+    $height: common.baseSize(25);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: common.baseSize(25);
+    padding: 0 common.baseSize(15);
+    font-size: common.baseSize(12);
     border: 1px solid #ccc;
-    border-radius: 15px;
+    border-radius: $height / 2;
   }
-  .w {
-    color: rgb(84, 163, 5);
+  &_icon {
+    font-size: common.baseSize(18);
+    margin-right: common.baseSize(8);
+  }
+  .weixin {
+    color: #04c904;
   }
 }
 .keeps {
-  border-top: 5px solid #ddd;
-  padding: 0 15px;
-  > h2 {
-    line-height: 50px;
+  border-top: 5px solid #e4e4e4;
+  padding-bottom: common.baseSize(30);
+  h2 {
+    line-height: common.baseSize(50);
+    font-size: common.baseSize(20);
     text-align: center;
   }
-  .item {
-    padding: 10px 0;
+  .comment {
+    padding: common.baseSize(10) common.baseSize(14);
     border-bottom: 1px solid #ccc;
-    .head {
+    &_head {
       display: flex;
       justify-content: space-between;
       align-items: center;
-      > img {
-        width: 50/360 * 100vw;
-        height: 50/360 * 100vw;
-        display: block;
+      h3 {
+        font-size: common.baseSize(14);
+      }
+      img {
+        $size: common.baseSize(50);
+        width: $size;
+        height: $size;
         border-radius: 50%;
       }
-      > div {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        margin-left: 10px;
-        > span {
-          font-size: 12px;
-          color: #999;
-          line-height: 25px;
-        }
-      }
-      > span {
+      span {
+        font-size: common.baseSize(12);
         color: #999;
-        font-size: 13px;
+        line-height: common.baseSize(25);
       }
     }
-    .text {
-      font-size: 14px;
-      color: #333;
-      padding: 20px 0 10px 0;
+    &_user-info {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      margin-left: common.baseSize(10);
+    }
+    &_text {
+      padding: common.baseSize(20) 0 common.baseSize(10) 0;
     }
   }
   .more {
-    width: 100px;
-    height: 30px;
-    line-height: 30px;
-    text-align: center;
-    border: 1px solid #ccc;
-    border-radius: 15px;
-    margin: 20px auto;
-    font-size: 13px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: common.baseSize(120);
+    height: common.baseSize(30);
+    margin: common.baseSize(25) auto;
+    border: 1px solid #797979;
+    background-color: #f2f2f2;
+    font-size: common.baseSize(14);
+  }
+}
+.leave-message {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  align-items: center;
+  width: 100%;
+  padding: common.baseSize(9) common.baseSize(16);
+  background-color: #f2f2f2;
+
+  input, button {
+    border: none;
+  }
+
+  input {
+    $height: common.baseSize(30);
+    padding: common.baseSize(8) common.baseSize(20);
+    height: $height;
+    border-radius: $height / 2;
+    font-size: common.baseSize(14);
+    color: #333;
+    background-color: #d7d7d7;
+  }
+  .btn-group {
+    display: flex;
+    button {
+      padding: 0;
+      margin-left: common.baseSize(18);
+      font-size: common.baseSize(20);
+      color: #7c7c7c;
+    }
   }
 }
 </style>
